2996b943a6e05f8203ca42061c3c55d26dfcf6a8
Wolf <zhao.haiyang@fruitsvending.com>
2020-05-26 13:40:08 +0800

    modify path

--- BatchRename/Form1.Designer.cs
+++ BatchRename/Form1.Designer.cs
@@ -37,9 +37,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(402, 9);
+            this.button1.Location = new System.Drawing.Point(402, 10);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(75, 25);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -47,40 +47,40 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 63);
+            this.textBox1.Location = new System.Drawing.Point(12, 68);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(384, 368);
+            this.textBox1.Size = new System.Drawing.Size(384, 398);
             this.textBox1.TabIndex = 1;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(12, 9);
+            this.textBox2.Location = new System.Drawing.Point(12, 10);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(384, 21);
+            this.textBox2.Size = new System.Drawing.Size(384, 20);
             this.textBox2.TabIndex = 2;
-            this.textBox2.Text = "E:\\Mega.nz\\Porn\\贱犬小狼的公厕第一季";
+            this.textBox2.Text = "E:\\Mega.nz\\";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(12, 36);
+            this.textBox3.Location = new System.Drawing.Point(12, 39);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(278, 21);
+            this.textBox3.Size = new System.Drawing.Size(278, 20);
             this.textBox3.TabIndex = 3;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(296, 36);
+            this.textBox4.Location = new System.Drawing.Point(296, 39);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 21);
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
             this.textBox4.TabIndex = 4;
             this.textBox4.Text = ".jpg";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(496, 448);
+            this.ClientSize = new System.Drawing.Size(496, 485);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
